3b3d91e9a57831140bfd3c073d22917f1eebb3ab
Dhruv Raikwar <dratarra2007@gmail.com>
2026-03-02 11:49:17 +0530

    Initial commit

--- src/components/MainSlider.jsx
+++ src/components/MainSlider.jsx
@@ -4,7 +4,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { FaStar } from "react-icons/fa6";
 
-import Img from "./image";
+import Img from "./image/pic.jpg";
 import Data from "./Data";
 function MainSlider() {
   var settings = {
